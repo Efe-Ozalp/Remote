4c13ad474be3b3982ca1fd46817b7f4b8c2d3210
Efe Ozalp <efeozalp0101@gmail.com>
2023-09-18 10:35:36 -0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Efe Ozalp on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
